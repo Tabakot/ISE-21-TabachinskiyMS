78ed7bb15d79b584e5f68d3073d59a377cd985c6
Tabakot <misha-tabach@mail.ru>
2019-12-05 21:09:01 +0400

    File and Stram

---
+++ FormPlaneConfig.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechProgWin
+{
+    public partial class FormPlaneConfig : Form
+    {
+
+        /// <summary>
+        /// Переменная-выбранный самолет
+        /// </summary>
+        ITransport plane = null;
+
+        /// <summary>
+        /// Событие
+        /// </summary>
+        private event planeDelegate eventAddPlane;
+
+        public FormPlaneConfig()
+        {
+            InitializeComponent();
+            panelBlack.MouseDown += panelColor_MouseDown;
+            panelOrange.MouseDown += panelColor_MouseDown;
+            panelPurple.MouseDown += panelColor_MouseDown;
+            panelGreen.MouseDown += panelColor_MouseDown;
+            panelRed.MouseDown += panelColor_MouseDown;
+            panelWhite.MouseDown += panelColor_MouseDown;
+            panelYellow.MouseDown += panelColor_MouseDown;
+            panelBlue.MouseDown += panelColor_MouseDown;
+
+            buttonCancel.Click += (object sender, EventArgs e) => { Close(); };
+        }
+
+        /// <summary>
+        /// Отрисовать самолет
+        /// </summary>
+        private void DrawPlane()
+        {
+            if (plane != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxPlane.Width, pictureBoxPlane.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                plane.SetPosition(5, 5, pictureBoxPlane.Width, pictureBoxPlane.Height);
+                plane.DrawPlane(gr);
+                pictureBoxPlane.Image = bmp;
+            }
+        }
+
+        /// <summary>
+        /// Добавление события
+        /// </summary>
+        /// <param name="ev"></param>
+        public void AddEvent(planeDelegate ev)
+        {
+            if (eventAddPlane == null)
+            {
+                eventAddPlane = new planeDelegate(ev);
+            }
+            else
+            {
+                eventAddPlane += ev;
+            }
+        }
+
+        /// <summary>
+        /// Передаем информацию при нажатии на Label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelPlane_MouseDown(object sender, MouseEventArgs e)
+        {
+            labelPlane.DoDragDrop(labelPlane.Text, DragDropEffects.Move |
+                DragDropEffects.Copy);
+        }
+
+        /// <summary>
+        /// Передаем информацию при нажатии на Label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelSeaplane_MouseDown(object sender, MouseEventArgs e)
+        {
+            labelSeaplane.DoDragDrop(labelSeaplane.Text, DragDropEffects.Move |
+                DragDropEffects.Copy);
+        }
+
+        /// <summary>
+        /// Проверка получаемой информации (ее типа на соответствие требуемому)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelPlane_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+        /// <summary>
+        /// Действия при приеме перетаскиваемой информации
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelPlane_DragDrop(object sender, DragEventArgs e)
+        {
+            switch (e.Data.GetData(DataFormats.Text).ToString())
+            {
+                case "Plane":
+                    plane = new Plane(100, 500, Color.White);
+                    break;
+                case "Seaplane":
+                    plane = new Seaplane(100, 500, Color.White, Color.Black, 10, false, true, false);
+                    break;
+            }
+            DrawPlane();
+
+        }
+
+        /// <summary>
+        /// Отправляем цвет с панели
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panelColor_MouseDown(object sender, MouseEventArgs e)
+        {
+            (sender as Control).DoDragDrop((sender as Control).BackColor,
+           DragDropEffects.Move | DragDropEffects.Copy);
+        }
+
+
+        /// <summary>
+        /// Проверка получаемой информации (ее типа на соответствие требуемому)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelMainColor_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Color)))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// Принимаем дополнительный цвет
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelMainColor_DragDrop(object sender, DragEventArgs e)
+        {
+            if (plane != null)
+            {
+                plane.SetMainColor((Color)e.Data.GetData(typeof(Color)));
+                DrawPlane();
+            }
+        }
+
+        /// <summary>
+        /// Проверка получаемой информации (ее типа на соответствие требуемому)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelOtherColor_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Color)))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// Принимаем основной цвет
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelOtherColor_DragDrop(object sender, DragEventArgs e)
+        {
+            if (plane != null)
+            {
+                if (plane is Seaplane)
+                {
+                    (plane as
+                   Seaplane).SetDopColor((Color)e.Data.GetData(typeof(Color)));
+                    DrawPlane();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Добавление самолета
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            eventAddPlane?.Invoke(plane);
+            Close();
+        }
+    }
+}
