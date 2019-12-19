f2ca1fbf06c40e35ceb7b3995d94e4d4f6272ee9
Tabakot <misha-tabach@mail.ru>
2019-12-20 01:50:47 +0400

    homework

---
+++ CountEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechProgWin
+{
+    public enum CountEngine
+    {
+        Four = 4,
+        Five = 5,
+        Six = 6
+    }
+}
